d0c823cb581349a9e2859e45a2a94d728ca613da
조재국 <jaeguk-cho@nhn-commerce.com>
2023-01-24 22:31:18 +0900

    array deque 만 사용

--- src/jaeguk-cho/230124/크레인 인형뽑기 게임.kt
+++ src/jaeguk-cho/230124/크레인 인형뽑기 게임.kt
@@ -1,23 +1,20 @@
-import java.util.*
-import kotlin.collections.ArrayDeque
-
 /**
  * @author Jaeguk Cho
  */
 
 class Solution {
     fun solution(board: Array<IntArray>, moves: IntArray): Int {
-        val map = List(board.first().size) { x -> ArrayDeque(board.map { it[x] }.filter { it > 0 }) }
-        val basket = Stack<Int>()
+        val map = List(board.random().size) { x -> ArrayDeque(board.map { it[x] }.filter { it > 0 }) }
+        val basket = ArrayDeque<Int>()
         var ans = 0
 
         moves.forEach { move ->
             map[move - 1].removeFirstOrNull()?.also {
-                if (basket.isNotEmpty() && basket.peek() == it) {
-                    basket.pop()
+                if (basket.lastOrNull() == it) {
+                    basket.removeLast()
                     ans += 2
                 } else {
-                    basket.push(it)
+                    basket.add(it)
                 }
             }
         }

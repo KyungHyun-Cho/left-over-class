5bee8afc88c7610b26223314701b3c2610c58572
조재국 <jaeguk-cho@nhn-commerce.com>
2023-01-24 22:59:03 +0900

    PR 반영

--- src/jaeguk-cho/230124/크레인 인형뽑기 게임.kt
+++ src/jaeguk-cho/230124/크레인 인형뽑기 게임.kt
@@ -4,12 +4,12 @@
 
 class Solution {
     fun solution(board: Array<IntArray>, moves: IntArray): Int {
-        val map = List(board.random().size) { x -> ArrayDeque(board.map { it[x] }.filter { it > 0 }) }
+        val map = List(board.size) { x -> ArrayDeque(board.map { it[x] }.filter { it > 0 }) }
         val basket = ArrayDeque<Int>()
         var ans = 0
 
         moves.forEach { move ->
-            map[move - 1].removeFirstOrNull()?.also {
+            map[move.dec()].removeFirstOrNull()?.also {
                 if (basket.lastOrNull() == it) {
                     basket.removeLast()
                     ans += 2

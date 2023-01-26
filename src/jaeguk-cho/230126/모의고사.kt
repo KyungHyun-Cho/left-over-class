c5033974a8965d676401f8bcd045b1876806936c
조재국 <jaeguk-cho@nhn-commerce.com>
2023-01-26 22:35:03 +0900

    더깔끔하게

--- src/jaeguk-cho/230126/모의고사.kt
+++ src/jaeguk-cho/230126/모의고사.kt
@@ -9,10 +9,10 @@ class `모의고사` {
 
     fun solution(answers: IntArray): List<Int> {
         val ans = listOf(
-            listOf(1, 2, 3, 4, 5).asSequence().repeat(),
-            listOf(2, 1, 2, 3, 2, 4, 2, 5).asSequence().repeat(),
-            listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5).asSequence().repeat()
-        ).map { it.take(answers.size).toList().zip(answers.toList(), Int::minus).count { it == 0 } }
+            listOf(1, 2, 3, 4, 5),
+            listOf(2, 1, 2, 3, 2, 4, 2, 5),
+            listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        ).map { it.asSequence().repeat().take(answers.size).zip(answers.asSequence(), Int::equals).count { it } }
 
         val max = ans.maxOf { it }
         return ans.withIndex().filter { it.value == max }.map { it.index + 1 }

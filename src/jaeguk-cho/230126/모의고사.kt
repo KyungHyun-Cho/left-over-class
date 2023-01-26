2654a7eb75fb670626db0a0cb60baf1008a83cf2
조재국 <jaeguk-cho@nhn-commerce.com>
2023-01-26 22:29:49 +0900

    리팩토링?

--- src/jaeguk-cho/230126/모의고사.kt
+++ src/jaeguk-cho/230126/모의고사.kt
@@ -5,19 +5,14 @@ package `jaeguk-cho`.`모의고사`
  */
 
 class `모의고사` {
+    fun Sequence<Int>.repeat() = sequence { while (true) yieldAll(this@repeat) }
+
     fun solution(answers: IntArray): List<Int> {
-        val seq1 = listOf(1, 2, 3, 4, 5)
-        val seq2 = listOf(2, 1, 2, 3, 2, 4, 2, 5)
-        val seq3 = listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
-        val ans = mutableListOf(0, 0, 0)
-
-        answers.forEachIndexed { i, q ->
-            if (q == seq1[i % 5]) ans[0]++
-            if (q == seq2[i % 8]) ans[1]++
-            if (q == seq3[i % 10]) ans[2]++
-        }
-
-        println(ans)
+        val ans = listOf(
+            listOf(1, 2, 3, 4, 5).asSequence().repeat(),
+            listOf(2, 1, 2, 3, 2, 4, 2, 5).asSequence().repeat(),
+            listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5).asSequence().repeat()
+        ).map { it.take(answers.size).toList().zip(answers.toList(), Int::minus).count { it == 0 } }
 
         val max = ans.maxOf { it }
         return ans.withIndex().filter { it.value == max }.map { it.index + 1 }

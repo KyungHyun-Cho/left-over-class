0a3de93f61c651b585aeb1e089830ed1ee7741a1
blucky8649 <blucky4511@gmail.com>
2023-01-26 22:39:00 +0900

    모의고사아아

---
+++ src/dongyeon-lee/230126/P42840.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(answers: IntArray): IntArray {
+        var answer = intArrayOf()
+        val one = listOf(1, 2, 3, 4, 5)
+        val two = listOf(2, 1, 2, 3, 2, 4, 2, 5)
+        val three = listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        return answers.mapIndexed { index, ans ->
+            listOf(one, two, three).map {
+                (index % it.size).let { cursor ->
+                    if (it[cursor] == ans) 1 else 0
+                }
+            }
+        }.reduce { acc, ints ->
+            acc.mapIndexed { index, i ->
+                acc[index] + ints[index]
+            }
+        }.let {
+            val max = it.maxOrNull()
+            it.mapIndexed { index, score ->
+                if (score == max) index.inc() else -100
+            }
+        }.filter { it > 0 }.toIntArray()
+    }
+}

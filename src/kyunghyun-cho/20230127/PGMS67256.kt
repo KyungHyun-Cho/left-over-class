b025ec89493687138544d757284b11de281ba9ea
조경현 <jkh9615@naver.com>
2023-01-27 22:12:12 +0900

    67256 refactoring - 1

--- src/kyunghyun-cho/20230127/PGMS67256.kt
+++ src/kyunghyun-cho/20230127/PGMS67256.kt
@@ -12,7 +12,6 @@ fun main() {
 }
 
 fun solution(numbers: IntArray, hand: String): String {
-    val handType = HAND.valueOf(hand.uppercase())
     var lPos = 10
     var rPos = 12
 
@@ -21,33 +20,20 @@ fun solution(numbers: IntArray, hand: String): String {
             1, 4, 7 -> {lPos = number; "L"}
             3, 6, 9 -> {rPos = number; "R"}
             else -> {
-                val lDist = getDist(lPos.toXY(), number.toXY())
-                val rDist = getDist(rPos.toXY(), number.toXY())
-                if(lDist < rDist){
-                    lPos = number
-                    "L"
-                }else if(lDist == rDist && handType == HAND.LEFT){
-                    lPos = number
-                    "L"
-                }else if(lDist == rDist && handType == HAND.RIGHT){
-                    rPos = number
-                    "R"
-                }else{
-                    rPos = number
-                    "R"
+                val lDist = lPos dist number
+                val rDist = rPos dist number
+                when{
+                    lDist < rDist -> {lPos = number; "L"}
+                    lDist == rDist && hand == "left" -> {lPos = number; "L"}
+                    else -> {rPos = number; "R"}
                 }
             }
         }
     }
 }
 
-fun Int.toXY(): Pair<Int, Int>{
-    return if(this == 0) 3 to 1 else (this-1) / 3 to (this-1) % 3
-}
+infix fun Int.dist(other: Int) = getDist(this.toXY(), other.toXY())
+
+fun Int.toXY() = if(this == 0) 3 to 1 else (this-1) / 3 to (this-1) % 3
 
 fun getDist(srcXY: Pair<Int, Int>, destXY: Pair<Int, Int>) = abs(srcXY.first - destXY.first) + abs(srcXY.second - destXY.second)
-
-
-enum class HAND {
-    LEFT, RIGHT
-}

5e58c28fa583617c2097974b3300bf5f78d5e112
blucky8649 <blucky4511@gmail.com>
2023-02-01 21:30:10 +0900

    toList() 붙임

--- src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
+++ src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
@@ -2,7 +2,7 @@ package `dongyeon-lee`.`230201`
 
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
-        return (6 - (lottos - win_nums).size).let {
+        return (6 - (lottos.toList() - win_nums.toList()).size).let {
             intArrayOf(calc(it + lottos.count { it == 0 }), calc(it))
         }
     }

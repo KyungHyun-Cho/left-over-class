fda5788c2b005f9329943fe21f9c26f3155a4e3d
blucky8649 <blucky4511@gmail.com>
2023-02-01 21:39:35 +0900

    조금 짧아짐

--- src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
+++ src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
@@ -1,10 +1,7 @@
 package `dongyeon-lee`.`230201`
 
 class Solution {
-    fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
-        return (6 - (lottos.toList() - win_nums.toList()).size).let {
-            intArrayOf(calc(it + lottos.count { it == 0 }), calc(it))
-        }
-    }
+    fun solution(lottos: IntArray, win_nums: IntArray): IntArray =
+        (6 - (lottos.count { it in win_nums })).let { intArrayOf(calc(it + lottos.count { it == 0 }), calc(it)) }
     fun calc(score: Int) = if (score <= 1) 6 else 7 - score
 }

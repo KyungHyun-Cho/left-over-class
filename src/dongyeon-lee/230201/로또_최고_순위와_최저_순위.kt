fe6d06353eccf5d11e0ab33229d012208e49e9b4
Lee Dong-Yeon <83625797+blucky8649@users.noreply.github.com>
2023-02-01 22:30:42 +0900

    Update src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
    반영
    
    Co-authored-by: 조갱 <76102680+KyungHyun-Cho@users.noreply.github.com>

--- src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
+++ src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
@@ -2,7 +2,7 @@ package `dongyeon-lee`.`230201`
 
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray) =
-        (6 - (lottos.count { it !in win_nums })).let { minScore ->
+        lottos.count { it in win_nums }.let { minScore ->
             val maxScore = minScore + lottos.count { it == 0 }
             listOf(maxScore, minScore).map { it.toRank() }
         }

cfa8b17af9f24f231b758a8e5c688d65508ce46d
blucky8649 <blucky4511@gmail.com>
2023-02-01 21:43:08 +0900

    어익후 실수

--- src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
+++ src/dongyeon-lee/230201/로또_최고_순위와_최저_순위.kt
@@ -2,6 +2,6 @@ package `dongyeon-lee`.`230201`
 
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray =
-        (6 - (lottos.count { it in win_nums })).let { intArrayOf(calc(it + lottos.count { it == 0 }), calc(it)) }
+        (6 - (lottos.count { it !in win_nums })).let { intArrayOf(calc(it + lottos.count { it == 0 }), calc(it)) }
     fun calc(score: Int) = if (score <= 1) 6 else 7 - score
 }

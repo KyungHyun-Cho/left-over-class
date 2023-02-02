f87e208100dcfdacd6f02bf4f86f2f5f153ffec8
blucky8649 <blucky4511@gmail.com>
2023-02-02 21:21:14 +0900

    방금 커밋은 무시해주세요

--- src/dongyeon-lee/230202/P81301.kt
+++ src/dongyeon-lee/230202/P81301.kt
@@ -3,7 +3,6 @@ package `dongyeon-lee`.`230202`
 class Solution {
     fun solution(s: String) =
         listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight" , "nine").foldIndexed(s) { index, acc, str ->
-                acc.replace(str, index.toString())
-            }
+            acc.replace(str, index.toString())
         }.toInt()
 }

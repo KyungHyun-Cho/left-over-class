d1c20ed6fc708d3a3f4664b92d63938fb10e26c2
blucky8649 <blucky4511@gmail.com>
2023-02-02 21:17:54 +0900

    재국님 피드백 반영

--- src/dongyeon-lee/230202/P81301.kt
+++ src/dongyeon-lee/230202/P81301.kt
@@ -2,8 +2,7 @@ package `dongyeon-lee`.`230202`
 
 class Solution {
     fun solution(s: String) =
-        listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight" , "nine").also { numList ->
-            numList.foldIndexed(s) { index, acc, str ->
+        listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight" , "nine").foldIndexed(s) { index, acc, str ->
                 acc.replace(str, index.toString())
             }
         }.toInt()

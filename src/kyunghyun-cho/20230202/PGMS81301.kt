d5ad6d6dfcc76a07ab6aea502379f125c4b38b38
조경현 <jkh9615@naver.com>
2023-02-02 21:34:12 +0900

    pr반영

--- src/kyunghyun-cho/20230202/PGMS81301.kt
+++ src/kyunghyun-cho/20230202/PGMS81301.kt
@@ -13,15 +13,14 @@ fun main() {
 }
 
 fun solution(s: String): Int {
-    val map = mapOf("zero" to "0", "one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9")
     var answer = s
-    map.forEach { (eng, num) ->
-        answer = answer.replace(eng.toRegex(), num)
+    listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine").forEachIndexed { idx, eng ->
+        answer = answer.replace(eng.toRegex(), idx.toString())
     }
     return answer.toInt()
 }
 
 fun solutionRegex(s: String): Int {
-    val map = mapOf("zero" to "0", "one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9", "\\d" to "AnyNumber")
-    return map.keys.joinToString("|").toRegex().findAll(s).toList().joinToString("") { map[it.value] ?: it.value }.toInt()
+    val numByEng = mapOf("zero" to "0", "one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9", "\\d" to "AnyNumber")
+    return numByEng.keys.joinToString("|").toRegex().findAll(s).joinToString("") { numByEng[it.value] ?: it.value }.toInt()
 }

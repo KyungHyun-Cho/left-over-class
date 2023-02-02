161cf9233f351ffef0d2954e1c90597a057ff171
blucky8649 <blucky4511@gmail.com>
2023-02-02 21:15:14 +0900

    let을 왜쓰냐 멍충아

--- src/dongyeon-lee/230202/P81301.kt
+++ src/dongyeon-lee/230202/P81301.kt
@@ -2,7 +2,7 @@ package `dongyeon-lee`.`230202`
 
 class Solution {
     fun solution(s: String) =
-        listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight" , "nine").let { numList ->
+        listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight" , "nine").also { numList ->
             numList.foldIndexed(s) { index, acc, str ->
                 acc.replace(str, index.toString())
             }

194c6c474be5aa344d5c92c5f9ae051fe4b44fdb
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-02 21:15:50 +0900

    동연님보다 먼저 커밋

--- src/jaeguk-cho/230202/숫자 문자열과 영단어.kt
+++ src/jaeguk-cho/230202/숫자 문자열과 영단어.kt
@@ -5,18 +5,6 @@ package `jaeguk-cho`.`숫자 문자열과 영단어`
  */
 
 class `숫자 문자열과 영단어` {
-    val dictionary = listOf(
-        "zero" to "0",
-        "one" to "1",
-        "two" to "2",
-        "three" to "3",
-        "four" to "4",
-        "five" to "5",
-        "six" to "6",
-        "seven" to "7",
-        "eight" to "8",
-        "nine" to "9",
-    )
-
-    fun solution(s: String) = dictionary.fold(s) { s, word -> s.replace(word.first, word.second) }.toInt()
+    fun solution(s: String) = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        .foldIndexed(s) { idx, s, word -> s.replace(word, idx.toString()) }.toInt()
 }

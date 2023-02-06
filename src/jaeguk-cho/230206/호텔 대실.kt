2d79e55d1b9b79afaf3b90579d70d18371935d72
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-06 23:04:24 +0900

    to time interval ~

--- src/jaeguk-cho/230206/호텔 대실.kt
+++ src/jaeguk-cho/230206/호텔 대실.kt
@@ -6,10 +6,10 @@ package `jaeguk-cho`.`호텔 대실`
 
 class `호텔 대실` {
     fun solution(book_time: Array<Array<String>>): Int {
-        val time = MutableList(2400) { 0 }
+        val time = MutableList(1450) { 0 }
         book_time.forEach { (s, e) ->
-            time[s.filter { it != ':' }.toInt()]++
-            time[e.filter { it != ':' }.toInt() + 10]--
+            time[s.split(':').let { it[0].toInt() * 60 + it[1].toInt() }]++
+            time[e.split(':').let { it[0].toInt() * 60 + it[1].toInt() + 10 }]--
         }
 
         return time.runningReduce(Int::plus).maxOf { it }

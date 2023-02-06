5f6d56b786baa96d449c85e34365e301334ef3cc
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-06 22:30:57 +0900

    나 스스로 한 수정

--- src/jaeguk-cho/230206/호텔 대실.kt
+++ src/jaeguk-cho/230206/호텔 대실.kt
@@ -7,9 +7,9 @@ package `jaeguk-cho`.`호텔 대실`
 class `호텔 대실` {
     fun solution(book_time: Array<Array<String>>): Int {
         val time = MutableList(2400) { 0 }
-        book_time.forEach {
-            time[it[0].filter { it != ':' }.toInt()]++
-            time[it[1].filter { it != ':' }.toInt()]--
+        book_time.forEach { (s, e) ->
+            time[s.filter { it != ':' }.toInt()]++
+            time[e.filter { it != ':' }.toInt() + 10]--
         }
 
         return time.runningReduce(Int::plus).maxOf { it }

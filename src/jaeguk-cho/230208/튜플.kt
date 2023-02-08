da38f3590e7eaf28eb83837e61582bd42d51ac48
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-08 22:47:39 +0900

    몹쓸코드

--- src/jaeguk-cho/230208/튜플.kt
+++ src/jaeguk-cho/230208/튜플.kt
@@ -5,8 +5,5 @@ package `jaeguk-cho`.`튜플`
  */
 
 class '튜플' {
-    fun solution(s: String): List<Int> {
-        val data = s.drop(2).dropLast(2).split("},{").map { it.split(',').map { it.toInt() } } + listOf(listOf())
-        return data.sortedBy { -it.size }.zipWithNext(List<Int>::minus).flatten().reversed()
-    }
+    fun solution(s: String) = s.drop(2).dropLast(2).split("},{").map { it.split(',') }.sortedBy { it.size }.fold(setOf(), Set<String>::union).map(String::toInt)
 }

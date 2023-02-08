11a367452d8daf52785dc3a24e885460c236fcf1
blucky8649 <blucky4511@gmail.com>
2023-02-08 22:00:03 +0900

    튜쁘르><

---
+++ src/dongyeon-lee/230208/P64065.kt
@@ -0,0 +1,14 @@
+package `dongyeon-lee`.`230208`
+
+class Solution {
+    fun solution(s: String): IntArray {
+        var prevList = listOf<Int>()
+        return s.substring(1, s.length - 1).split("},{").map {
+            it.replace("{", "").replace("}", "").split(",").map { it.toInt() }
+        }.sortedBy { it.size }.map {
+            val remainder = if (prevList.isEmpty()) { it.first() } else { (it - prevList).first() }
+            prevList = it
+            remainder
+        }.toIntArray()
+    }
+}

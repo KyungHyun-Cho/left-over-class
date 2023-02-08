84bd78a641436d218ca3534d6737f47ceaac38e6
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-08 22:03:49 +0900

    typo

--- src/jaeguk-cho/230208/튜플.kt
+++ src/jaeguk-cho/230208/튜플.kt
@@ -1,4 +1,4 @@
-package `jaeguk-cho`.`호텔 대실`
+package `jaeguk-cho`.`튜플`
 
 /**
  * @author Jaeguk Cho
@@ -7,6 +7,6 @@ package `jaeguk-cho`.`호텔 대실`
 class '튜플' {
     fun solution(s: String): List<Int> {
         val data = s.drop(2).dropLast(2).split("},{").map { it.split(',').map { it.toInt() } } + listOf(listOf())
-        return플data.sortedBy { it.size }.zipWithNext { a, b -> b - a }.flatten()
+        return data.sortedBy { it.size }.zipWithNext { a, b -> b - a }.flatten()
     }
 }

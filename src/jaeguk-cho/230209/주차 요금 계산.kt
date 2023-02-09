d93f39bc375c6c27e0e8199fc520fc967241826e
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-09 21:29:04 +0900

    second commit

--- src/jaeguk-cho/230209/주차 요금 계산.kt
+++ src/jaeguk-cho/230209/주차 요금 계산.kt
@@ -6,15 +6,19 @@ package `jaeguk-cho`.`주차 요금 계산`
 
 class `주차 요금 계산` {
     fun solution(fees: IntArray, records: Array<String>): List<Int> {
-        return (0..10000).map { num ->
-            records.filter { it.substringAfter(' ').substringBefore(' ').toInt() == num }
+        return (0 until 10000).map { carNo ->
+            records.filter { it.substringAfter(' ').substringBefore(' ').toInt() == carNo }
                 .map { it.substringBefore(' ').toUnit() }
                 .let { if (it.size % 2 == 1) it + 1439 else it }
                 .let { it.chunked(2) { -it.reduce(Int::minus) }.sum() }
-        }.filter { it > 0 }.map { fees[1] + if (it <= fees[0]) 0 else (it - fees[0] + fees[2] - 1) / fees[2] * fees[3] }
+        }.filter { it > 0 }.map { calculate(fees, it) }
+    }
+
+    fun calculate(fees: IntArray, time: Int): Int {
+        return fees[1] + if (time <= fees[0]) 0 else (time - fees[0] + fees[2] - 1) / fees[2] * fees[3]
     }
 
     fun String.toUnit(): Int {
-        return split(':').map { it.toInt() }.let { (h, m) -> 60 * h + m }
+        return split(':').map { it.toInt() }.let { (h, m) -> h * 60 + m }
     }
 }

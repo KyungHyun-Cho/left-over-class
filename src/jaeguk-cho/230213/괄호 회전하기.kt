26c0eb681bd6f7f685dff5f17254f9d8de4e013f
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-13 21:58:55 +0900

    클래스명 수정

--- src/jaeguk-cho/230213/괄호 회전하기.kt
+++ src/jaeguk-cho/230213/괄호 회전하기.kt
@@ -1,8 +1,10 @@
+package `jaeguk-cho`.`괄호 회전하기`
+
 /**
  * @author Jaeguk Cho
  */
 
-class Solution {
+class `괄호 회전하기` {
     fun solution(s: String) = s.indices.count { check(s.drop(it) + s.take(it)) }
 
     fun check(s: String) = with(ArrayDeque<Char>()) {

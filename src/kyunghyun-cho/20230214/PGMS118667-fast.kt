c3026562352a9c6ea52e1476420e4cf8e8f2e6b6
조경현 <jkh9615@naver.com>
2023-02-14 21:40:11 +0900

    더 간결한 버전 추가

--- src/kyunghyun-cho/20230214/PGMS118667-fast.kt
+++ src/kyunghyun-cho/20230214/PGMS118667-fast.kt
@@ -5,10 +5,11 @@ package `kyunghyun-cho`.`20230214`.`fast`
  */
 
 fun main() {
-    //val (queue1, queue2) = intArrayOf(3, 2, 7, 2) to intArrayOf(4, 6, 5, 1)
-    val (queue1, queue2) = intArrayOf(1, 2, 1, 2) to intArrayOf(1, 10, 1, 2)
+    val (queue1, queue2) = intArrayOf(3, 2, 7, 2) to intArrayOf(4, 6, 5, 1)
+    //val (queue1, queue2) = intArrayOf(1, 2, 1, 2) to intArrayOf(1, 10, 1, 2)
     println(solution(queue1, queue2))
     println(solution2(queue1, queue2))
+    println(solution3(queue1, queue2))
 }
 
 fun solution(queue1: IntArray, queue2: IntArray): Int {
@@ -30,7 +31,7 @@ fun solution2(queue1: IntArray, queue2: IntArray): Int {
     val (q1, q2) = (queue1 + queue2) to (queue2 + queue1)
     var (sum1, sum2) = queue1.sum().toLong() to queue2.sum().toLong()
     var (pos1, pos2) = 0 to 0
-    while (pos1 < q1.size && pos2 < q1.size ) {
+    while (pos1 < q1.size && pos2 < q1.size) {
         if (sum1 < sum2) {
             sum1 += q2[pos2]
             sum2 -= q2[pos2++]
@@ -38,6 +39,18 @@ fun solution2(queue1: IntArray, queue2: IntArray): Int {
             sum1 -= q1[pos1]
             sum2 += q1[pos1++]
         } else return pos1 + pos2
+    }
+    return -1
+}
+
+fun solution3(queue1: IntArray, queue2: IntArray): Int {
+    val (q1, q2) = (queue1 + queue2) to (queue2 + queue1)
+    var (sum1, sum2) = queue1.sum().toLong() to queue2.sum().toLong()
+    var (pos1, pos2) = 0 to 0
+    while (pos1 < q1.size && pos2 < q1.size) {
+        if (sum1 < sum2) q2[pos2++].also { sum1 += it; sum2 -= it }
+        else if (sum1 > sum2) q1[pos1++].also { sum1 -= it; sum2 += it }
+        else return pos1 + pos2
     }
     return -1
 }

dec35118282fe3f4ec60dbef7e037e896ff22a8a
조경현 <jkh9615@naver.com>
2023-02-15 22:41:25 +0900

    변수명이나 이쁘게,,

--- src/kyunghyun-cho/20230215/PGMS150368.kt
+++ src/kyunghyun-cho/20230215/PGMS150368.kt
@@ -15,8 +15,8 @@ fun main() {
         intArrayOf(27, 9200),
         intArrayOf(32, 6900))
     val emoticons = intArrayOf(1300, 1500, 1600, 4900)
-//    val users = arrayOf(intArrayOf(40, 10000), intArrayOf(25, 10000))
-//    val emoticons = intArrayOf(7000, 9000)
+    //    val users = arrayOf(intArrayOf(40, 10000), intArrayOf(25, 10000))
+    //    val emoticons = intArrayOf(7000, 9000)
 
     println(solution(users, emoticons).toList())
 }
@@ -24,29 +24,28 @@ fun main() {
 fun solution(users: Array<IntArray>, emoticons: IntArray): IntArray {
     val answer = intArrayOf(0, 0)
     combinationsWithRepetition(listOf(10, 20, 30, 40), emoticons.size).forEach { discountRates ->
-        users.map { (buyRate, subscribePrice) ->
-            subscribePrice to
-            discountRates.withIndex()
-                .filter { (_, discountRate) -> discountRate >= buyRate }
-                .sumOf { (idx, discountRate) -> emoticons[idx] * (100-discountRate) / 100 }
-        }.partition { (subscribePrice, paid) -> paid >= subscribePrice }
+        users.map { (minDiscountRateToBuy, minPriceToSubscribe) ->
+            minPriceToSubscribe to
+                discountRates.withIndex()
+                    .filter { (_, discountRate) -> discountRate >= minDiscountRateToBuy }
+                    .sumOf { (idx, discountRate) -> emoticons[idx] * (100 - discountRate) / 100 }
+        }.partition { (minPriceToSubscribe, paid) -> paid >= minPriceToSubscribe }
             .also { (subscribe, notSubscribe) ->
                 val subscriberCnt = subscribe.size
                 val notSubscribePaidSum = notSubscribe.sumOf { it.second }
-                if(subscriberCnt > answer[0] || (subscriberCnt >= answer[0] && notSubscribePaidSum >= answer[1])) {
+                if (subscriberCnt > answer[0] || (subscriberCnt >= answer[0] && notSubscribePaidSum >= answer[1])) {
                     answer[0] = subscriberCnt
                     answer[1] = notSubscribePaidSum
                 }
             }
     }
-
     return answer
 }
 
 fun combinationsWithRepetition(list: List<Int>, n: Int): List<List<Int>> {
     val result = mutableListOf<List<Int>>()
     val k = list.distinct().size
-    for (i in 0 until  k.toDouble().pow(n).toInt()) {
+    for (i in 0 until k.toDouble().pow(n).toInt()) {
         val indices = i.toString(k).padStart(n, '0').map { it.toString().toInt() }
         val combination = indices.map { list[it] }
         result.add(combination)

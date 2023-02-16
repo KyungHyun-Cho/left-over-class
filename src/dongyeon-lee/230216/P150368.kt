549e17df13e713003755cb0563f15eb245ac986d
blucky8649 <blucky4511@gmail.com>
2023-02-16 21:46:57 +0900

    replaceFirstChar

--- src/dongyeon-lee/230216/P150368.kt
+++ src/dongyeon-lee/230216/P150368.kt
@@ -1,3 +1,3 @@
 package `dongyeon-lee`.`230216`
 
-class Solution { fun solution(s: String) = s.lowercase().split(" ").joinToString(" ") { it.take(1).uppercase() + it.drop(1) } }
+class Solution { fun solution(s: String) = s.lowercase().split(" ").joinToString(" ") { it.replaceFirstChar { it.uppercase() } } }

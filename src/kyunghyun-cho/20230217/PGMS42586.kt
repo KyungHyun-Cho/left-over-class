0f9a65efc1f04169196257c13429a3c36d0952d4
조경현 <jkh9615@naver.com>
2023-02-17 21:38:11 +0900

    init commit

---
+++ src/kyunghyun-cho/20230217/PGMS42586.kt
@@ -0,0 +1,23 @@
+package `kyunghyun-cho`.`20230217`
+
+/**
+ * @author Kyunghyun Cho
+ */
+
+fun main() {
+    val (progresses, speeds) = intArrayOf(95, 90, 99, 99, 80, 99) to intArrayOf(1, 1, 1, 1, 1, 1) // 1, 3, 2
+    //val (progresses, speeds) = intArrayOf(93, 30, 55) to intArrayOf(1, 30, 5) // 2, 1
+    println(solution(progresses, speeds))
+}
+
+fun solution(progresses: IntArray, speeds: IntArray): List<Int>{
+    var lastMax = 0
+    return progresses.zip(speeds)
+        .map { (p, s) -> ((100 - p) + s - 1) / s }
+        .fold(mutableListOf()) { acc, value ->
+            acc.apply {
+                if (value > lastMax) this.add(1).also { lastMax = value }
+                 else this[acc.lastIndex]++
+            }
+        }
+}

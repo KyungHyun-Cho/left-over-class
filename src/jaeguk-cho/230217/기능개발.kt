491f431c6b5dea605b57c853b1168e0f89e42627
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-17 22:35:11 +0900

    pr 반영

--- src/jaeguk-cho/230217/기능개발.kt
+++ src/jaeguk-cho/230217/기능개발.kt
@@ -8,16 +8,15 @@ class `기능개발` {
     fun solution(progresses: IntArray, speeds: IntArray): List<Int> {
         val answer = mutableListOf<Int>()
         val stack = ArrayDeque<Int>()
-        val list = speeds.mapIndexed { i, n -> ((100 - progresses[i]) + n - 1) / n } + 105
 
-        list.forEach {
+        progresses.zip(speeds) { p, s -> (100 - p + s - 1) / s }.forEach {
             if (stack.isNotEmpty() && stack.first() < it) {
                 answer += stack.size
-                while (stack.isNotEmpty()) stack.removeLast()
+                stack.clear()
             }
             stack.add(it)
         }
 
-        return answer
+        return answer + stack.size
     }
 }

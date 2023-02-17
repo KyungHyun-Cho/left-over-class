a4de18047a3125f38489c90ee00178d4d76afe25
blucky8649 <blucky4511@gmail.com>
2023-02-17 23:07:32 +0900

    ceil, toDouble 제거..

--- src/dongyeon-lee/230217/P150368.kt
+++ src/dongyeon-lee/230217/P150368.kt
@@ -1,15 +1,11 @@
 package `dongyeon-lee`.`230217`
 
-import kotlin.math.ceil
-
 class Solution {
     fun solution(progresses: IntArray, speeds: IntArray) =
-        progresses.zip(speeds) { progress, speed ->
-            ceil((100 - progress).toDouble() / speed).toInt()
-        }.let { days ->
+        progresses.zip(speeds) { progress, speed -> ((100 - progress) + speed - 1) / speed }.let {days ->
             var prev = 0
             days.mapIndexed { index, day ->
                 if (index == 0 || day > prev) day.also { prev = it } else prev
             }
-        }.groupBy { it }.map { it.value.size }
+        }.groupingBy { it }.eachCount().values
 }

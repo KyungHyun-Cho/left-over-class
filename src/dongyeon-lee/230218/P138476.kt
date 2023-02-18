11bf202f1242121745219781102ec420e13084f5
blucky8649 <blucky4511@gmail.com>
2023-02-19 00:00:57 +0900

    pr 반영

--- src/dongyeon-lee/230218/P138476.kt
+++ src/dongyeon-lee/230218/P138476.kt
@@ -2,17 +2,10 @@ package `dongyeon-lee`.`230218`
 
 /* 귤  고르기 */
 class Solution {
-    fun solution(k: Int, tangerine: IntArray): Int {
-        var answer = 0
-        var count = 0
+    fun solution(k: Int, tangerine: IntArray) =
         tangerine.groupBy { it }
             .map { it.value.size }
             .sortedByDescending { it }
-            .onEach {
-                if (count < k) {
-                    count += it ; answer ++
-                }
-            }
-        return answer
-    }
+            .runningFold(0, Int::plus)
+            .indexOfFirst { k <= it }
 }

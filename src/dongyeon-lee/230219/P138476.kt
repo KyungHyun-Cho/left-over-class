84e9593ba375ede64463c84909797e90c43ba668
blucky8649 <blucky4511@gmail.com>
2023-02-21 23:02:42 +0900

    pr 반영

--- src/dongyeon-lee/230219/P138476.kt
+++ src/dongyeon-lee/230219/P138476.kt
@@ -2,6 +2,6 @@ package `dongyeon-lee`.`230219`
 
 /* H-INDEX */
 class Solution {
-    fun solution(citations: IntArray) = (0..(citations.size)).last { citation ->
+    fun solution(citations: IntArray) = (0..citations.size).last { citation ->
         citations.count { it >= citation } >= citation }
 }

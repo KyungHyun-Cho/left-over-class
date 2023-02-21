4ef69edced963eaf49f9c4c9a97589b74044e37b
blucky8649 <blucky4511@gmail.com>
2023-02-21 22:57:21 +0900

    last 사용 (범위 주의 : 정답이 0인 경우도 있다)

--- src/dongyeon-lee/230219/P138476.kt
+++ src/dongyeon-lee/230219/P138476.kt
@@ -2,8 +2,6 @@ package `dongyeon-lee`.`230219`
 
 /* H-INDEX */
 class Solution {
-    fun solution(citations: IntArray) = (1..(citations.maxOrNull() ?: 0)).maxOfOrNull { citation ->
-        val isCountOverCitation = citations.count { it >= citation } >= citation
-        if (isCountOverCitation) citation else 0
-    } ?: 0
+    fun solution(citations: IntArray) = (0..(citations.size)).last { citation ->
+        citations.count { it >= citation } >= citation }
 }

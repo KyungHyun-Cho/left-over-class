2bef8d9ba53c84cdd99e479f9c670abb0aeba176
조경현 <jkh9615@naver.com>
2023-02-21 21:19:36 +0900

    빠른버전

--- src/kyunghyun-cho/20230221/PGMS42747.kt
+++ src/kyunghyun-cho/20230221/PGMS42747.kt
@@ -7,9 +7,15 @@ package `kyunghyun-cho`.`20230221`
 fun main() {
     val citations = intArrayOf(3, 0, 6, 1, 5) // 3
     println(solution(citations))
+    println(fastSolution(citations))
 }
 
 fun solution(citations: IntArray) =
     citations.toSet()
         .filter { citation -> citations.count { it >= citation } == citations.count { it <= citation } }
         .maxOrNull()!!
+
+fun fastSolution(citations: IntArray) =
+    citations.toSortedSet()
+        .last { citation -> citations.count { it >= citation } == citations.count { it <= citation } }
+

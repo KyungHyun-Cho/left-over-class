bd7e3b834c17c688a52a00dec55a93d7cb5e3ee1
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-21 21:34:40 +0900

    😋

--- src/jaeguk-cho/230221/H-Index.kt
+++ src/jaeguk-cho/230221/H-Index.kt
@@ -5,6 +5,5 @@ package `jaeguk-cho`.`H-Index`
  */
 
 class `H-Index` {
-    fun solution(citations: IntArray) = (1..citations.size)
-        .filter { h -> citations.count { it >= h } >= h }.maxOf { it }
+    fun solution(citations: IntArray) = (1..citations.size).last { h -> citations.count { it >= h } >= h }
 }

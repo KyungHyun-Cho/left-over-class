d94fe2b610d95c3e0bbc04d8717dd2e2b5eba609
조경현 <jkh9615@naver.com>
2023-02-23 21:27:20 +0900

    라인 수나 짧게 가져가쟈,,

--- src/kyunghyun-cho/20230223/PGMS131127.kt
+++ src/kyunghyun-cho/20230223/PGMS131127.kt
@@ -4,10 +4,23 @@ package `kyunghyun-cho`.`20230223`
  * @author Kyunghyun Cho
  */
 
-fun main(){
+fun main() {
     val want = arrayOf("banana", "apple", "rice", "pork", "pot")
     val number = intArrayOf(3, 2, 2, 2, 1)
-    val discount = arrayOf("chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana")
+    val discount = arrayOf("chicken",
+        "apple",
+        "apple",
+        "banana",
+        "rice",
+        "apple",
+        "pork",
+        "banana",
+        "pork",
+        "rice",
+        "pot",
+        "banana",
+        "apple",
+        "banana")
 
     println(solution(want, number, discount))
     println(fastSolution(want, number, discount))
@@ -15,7 +28,7 @@ fun main(){
 
 fun solution(want: Array<String>, number: IntArray, discount: Array<String>): Int {
     val cntByWantItems = (want zip number.toList())
-    return discount.toList().windowed(10)
+    return discount.asSequence().windowed(10)
         .map { discountTargets -> discountTargets.groupingBy { it }.eachCount() }
         .count { discountTargets ->
             cntByWantItems.all { (wantItem, wantCnt) -> discountTargets.getOrDefault(wantItem, 0) >= wantCnt }
@@ -26,14 +39,10 @@ fun fastSolution(want: Array<String>, number: IntArray, discount: Array<String>)
     var answer = 0
     val cntByWantItems = (want zip number.toList())
     val discountMap = discount.take(9).groupingBy { it }.eachCount().toMutableMap()
-    (9 until discount.size).forEach{ idx ->
-        val target = discount[idx]
-        discountMap[target] = discountMap.getOrDefault(target, 0) + 1
-        if(cntByWantItems.all { (wantItem, wantCnt) -> discountMap.getOrDefault(wantItem, 0) >= wantCnt })
-            answer++
-        val deleteTarget = discount[idx - 9]
-        discountMap[deleteTarget] = discountMap.getValue(deleteTarget) - 1
+    (9 until discount.size).forEach { idx ->
+        discountMap[discount[idx]] = discountMap.getOrDefault(discount[idx], 0) + 1
+        if (cntByWantItems.all { (wantItem, wantCnt) -> discountMap.getOrDefault(wantItem, 0) >= wantCnt }) answer++
+        discountMap[discount[idx - 9]] = discountMap.getValue(discount[idx - 9]) - 1
     }
-
     return answer
 }

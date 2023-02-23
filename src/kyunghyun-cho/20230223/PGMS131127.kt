f1da5395b74a6cf24ed5354461e65945ef6bcdcd
조경현 <jkh9615@naver.com>
2023-02-23 21:10:50 +0900

    빠른버전?

--- src/kyunghyun-cho/20230223/PGMS131127.kt
+++ src/kyunghyun-cho/20230223/PGMS131127.kt
@@ -10,6 +10,7 @@ fun main(){
     val discount = arrayOf("chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana")
 
     println(solution(want, number, discount))
+    println(fastSolution(want, number, discount))
 }
 
 fun solution(want: Array<String>, number: IntArray, discount: Array<String>): Int {
@@ -19,4 +20,20 @@ fun solution(want: Array<String>, number: IntArray, discount: Array<String>): In
         .count { discountTargets ->
             cntByWantItems.all { (wantItem, wantCnt) -> discountTargets.getOrDefault(wantItem, 0) >= wantCnt }
         }
+}
+
+fun fastSolution(want: Array<String>, number: IntArray, discount: Array<String>): Int {
+    var answer = 0
+    val cntByWantItems = (want zip number.toList())
+    val discountMap = discount.take(9).groupingBy { it }.eachCount().toMutableMap()
+    (9 until discount.size).forEach{ idx ->
+        val target = discount[idx]
+        discountMap[target] = discountMap.getOrDefault(target, 0) + 1
+        if(cntByWantItems.all { (wantItem, wantCnt) -> discountMap.getOrDefault(wantItem, 0) >= wantCnt })
+            answer++
+        val deleteTarget = discount[idx - 9]
+        discountMap[deleteTarget] = discountMap.getValue(deleteTarget) - 1
+    }
+
+    return answer
 }

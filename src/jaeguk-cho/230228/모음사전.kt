56b3623c05431eed9ee90af68c3303cdc846c6b3
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-28 22:21:17 +0900

    PR 반영

--- src/jaeguk-cho/230228/모음사전.kt
+++ src/jaeguk-cho/230228/모음사전.kt
@@ -7,15 +7,10 @@ package `jaeguk-cho`.`모음사전`
 class `모음사전` {
     val vowels = listOf('A', 'E', 'I', 'O', 'U')
 
-    fun getByLength(length: Int, prefix: String = ""): List<String> {
+    fun getStringByLength(length: Int, prefix: String = ""): List<String> {
         if (length == 0) return listOf(prefix)
-        return vowels.flatMap { getByLength(length - 1, prefix + it) }
+        return vowels.flatMap { getStringByLength(length - 1, prefix + it) }
     }
 
-    fun getByLengthLessThanEqual(length: Int): List<String> {
-        if (length == 0) return listOf()
-        return getByLength(length) + getByLengthLessThanEqual(length - 1)
-    }
-
-    fun solution(word: String) = getByLengthLessThanEqual(5).sorted().indexOf(word) + 1
+    fun solution(word: String) = (1..5).flatMap { getStringByLength(it) }.sorted().indexOf(word) + 1
 }

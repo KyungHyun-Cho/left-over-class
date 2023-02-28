021233d17956db06e1e71cc12800c6b4c9b440e0
조재국 <jaeguk-cho@nhn-commerce.com>
2023-02-28 22:27:37 +0900

    PR 반영

--- src/jaeguk-cho/230228/모음사전.kt
+++ src/jaeguk-cho/230228/모음사전.kt
@@ -8,8 +8,7 @@ class `모음사전` {
     val vowels = listOf('A', 'E', 'I', 'O', 'U')
 
     fun getStringByLength(length: Int, prefix: String = ""): List<String> {
-        if (length == 0) return listOf(prefix)
-        return vowels.flatMap { getStringByLength(length - 1, prefix + it) }
+        return listOf(prefix).takeIf { length == 0 } ?: vowels.flatMap { getStringByLength(length - 1, prefix + it) }
     }
 
     fun solution(word: String) = (1..5).flatMap { getStringByLength(it) }.sorted().indexOf(word) + 1

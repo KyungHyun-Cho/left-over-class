84a953f0bc37d00dd6144ff0ac40deeb0b440628
조경현 <jkh9615@naver.com>
2023-03-01 21:28:37 +0900

    일단 최적화

--- src/kyunghyun-cho/20230301/PGMS42842.kt
+++ src/kyunghyun-cho/20230301/PGMS42842.kt
@@ -10,5 +10,8 @@ fun main() {
     println(solution(brown, yellow))
 }
 
-fun solution(brown: Int, yellow: Int) = (1..5000).asSequence().flatMap { f -> (1..5000).map { s -> f to s } }.filter { (f, s) -> f >= s }.filter { (f, s) -> f * s == brown + yellow }.first { (f, s) -> (f + s) * 2 - 4 == brown }.let { listOf(it.first, it.second) }
+fun solution(brown: Int, yellow: Int) =
+    (1..5000).asSequence().flatMap { f -> (1..f).map { s -> f to s } }
+        .first { (f, s) -> (f * s == brown + yellow) && ((f + s) * 2 - 4 == brown) }
+        .toList()
 

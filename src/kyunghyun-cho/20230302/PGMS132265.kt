9d26c2372de520c2334d54e659c22987372cb0ca
조경현 <jkh9615@naver.com>
2023-03-02 21:33:36 +0900

    코드 스타일 적용

--- src/kyunghyun-cho/20230302/PGMS132265.kt
+++ src/kyunghyun-cho/20230302/PGMS132265.kt
@@ -18,13 +18,13 @@ fun verySlowSolution(topping: IntArray): Int {
     return topping.indices.count { topping.take(it).toSet().size == topping.drop(it).toSet().size }
 }
 
-fun stillSlowSolution(topping: IntArray): Int{
+fun stillSlowSolution(topping: IntArray): Int {
     val one = ArrayDeque<Int>()
     val two = ArrayDeque<Int>().apply { this.addAll(topping.toList()) }
     var answer = 0
-    while(two.isNotEmpty()){
+    while (two.isNotEmpty()) {
         one.addLast(two.pollFirst())
-        if(one.toSet().size == two.toSet().size) answer++
+        if (one.toSet().size == two.toSet().size) answer++
     }
     return answer
 }
@@ -33,9 +33,9 @@ fun solution(topping: IntArray): Int {
     val one = topping.toList().groupingBy { it }.eachCount().toMutableMap()
     val two = mutableMapOf<Int, Int>()
 
-    return topping.fold(0){acc, i ->
+    return topping.fold(0) { acc, i ->
         two[i] = two.getOrDefault(i, 0) + 1
         if (one[i] == 1) one.remove(i) else one[i] = one.getValue(i) - 1
-        if(one.size == two.size) acc + 1 else acc
+        if (one.size == two.size) acc + 1 else acc
     }
 }

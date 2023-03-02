cf1e7fbf7c5f986c35b1624d59d4ec0b8df377f2
blucky8649 <blucky4511@gmail.com>
2023-03-02 22:28:08 +0900

    패키지명, 컴파일오 류 수정

--- src/dongyeon-lee/230302/P42842.kt
+++ src/dongyeon-lee/230302/P42842.kt
@@ -1,16 +1,14 @@
-package `dongyeon-lee`.`230223`
+package `dongyeon-lee`.`230302`
 
 /* 롤케이크 자르기 */
 class Solution {
     fun solution(topping: IntArray): Int {
         val chul = mutableSetOf<Int>()
-        val bro = topping.groupingBy { it }.eachCount().toMutableMap()
+        val bro = topping.toList().groupingBy { it }.eachCount().toMutableMap()
         return topping.fold(0) { acc, t ->
             chul.add(t)
-            if (bro.contains(t)) {
-                bro[t] = bro.getValue(t).minus(1)
-                if (bro.getValue(t) < 1) bro.remove(t)
-            }
+            bro[t] = bro.getValue(t).minus(1)
+            if (bro.getValue(t) < 1) bro.remove(t)
             if (chul.size == bro.size) acc.inc() else acc
         }
     }

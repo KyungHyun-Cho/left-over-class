d1b2e32d4927ee64e9d0f676f13570949ca6d2f1
조재국 <jaeguk-cho@nhn-commerce.com>
2023-03-07 08:37:03 +0900

    PR 반영

--- src/jaeguk-cho/230304/뒤에 있는 큰 수 찾기.kt
+++ src/jaeguk-cho/230304/뒤에 있는 큰 수 찾기.kt
@@ -7,13 +7,13 @@ package `jaeguk-cho`.`뒤에 있는 큰 수 찾기`
 class `뒤에 있는 큰 수 찾기` {
     fun solution(numbers: IntArray) = with(ArrayDeque<Pair<Int, Int>>()) {
         val answer = MutableList(numbers.size) { -1 }
-        add(Int.MAX_VALUE to -1)
+        addLast(Int.MAX_VALUE to -1)
 
         numbers.forEachIndexed { i, n ->
             while (last().first < n) {
                 removeLast().also { answer[it.second] = n }
             }
-            add(n to i)
+            addLast(n to i)
         }
 
         answer

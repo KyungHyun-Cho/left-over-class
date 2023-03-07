6d54cf401bb5496a05454533ba91999b36eed8a3
조재국 <jaeguk-cho@nhn-commerce.com>
2023-03-07 21:13:40 +0900

    initial commit

---
+++ src/main/kotlin/jaeguk-cho/230307/입국심사.kt
@@ -0,0 +1,26 @@
+package `jaeguk-cho`.`230307`
+
+/**
+ * @author Jaeguk Cho
+ */
+
+class Solution {
+    fun solution(n: Int, times: IntArray): Long {
+        fun immigration(m: Long) = n <= times.sumOf { m / it }
+
+        var ans = -1L
+        var l = 1L
+        var r = 1000000000000000000
+        while (l <= r) {
+            val m = (l + r) / 2
+            if (immigration(m)) {
+                ans = m
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        
+        return ans
+    }
+}

373564e2e0a6a12b64a66b449adae17406465ba3
조재국 <jaeguk-cho@nhn-commerce.com>
2023-03-09 20:56:58 +0900

    initial commit

---
+++ src/main/kotlin/jaeguk-cho/230309/징검다리 건너기.kt
@@ -0,0 +1,27 @@
+package `jaeguk-cho`.`230309`
+
+/**
+ * @author Jaeguk Cho
+ */
+
+class Solution {
+    fun solution(stones: IntArray, k: Int): Int {
+        var (l, r) = 1 to 200000000
+        var ans = (1..r).random()
+
+        fun check(m: Int) = stones.toList().windowed(k).none { it.all { it < m } }
+
+        while (l <= r) {
+            val m = (l + r) / 2
+
+            if (check(m)) {
+                ans = m
+                l = m + 1
+            } else {
+                r = m - 1
+            }
+        }
+
+        return ans
+    }
+}
